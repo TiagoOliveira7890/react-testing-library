2bfc26fc46231c29b199ddccd2ab6a819eec7736
Tiago Oliveira <pliveiratiago7890@gmail.com>
2022-11-24 22:15:28 -0300

    Requisito 4

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemon from '../pages/FavoritePokemon';
+import pokemonList from '../data';
+
+describe('Teste o componente <FavoritePokemon.js />', () => {
+  it('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos;', () => {
+    renderWithRouter(<FavoritePokemon />);
+
+    const noPokemon = screen.getByText(/no favorite pokémon found/i);
+
+    expect(noPokemon).toBeInTheDocument();
+  });
+
+  it('Teste se são exibidos todos os cards de Pokémon favoritados.', () => {
+    renderWithRouter(<FavoritePokemon pokemonList={ pokemonList } />);
+    const dragonair = screen.getByText(/dragonair/i);
+    expect(dragonair).toBeInTheDocument();
+  });
+});
